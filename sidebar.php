7905d06089a3698743613d2cadcbf154a700ebe4
@Ruke45 <madusankagmt@gmail.com>
2020-07-21 12:30:00 +0530

    changed UI

--- sidebar.php
+++ sidebar.php
@@ -16,15 +16,15 @@
         <div class="sidebar-brand-icon rotate-n-15">
             <i class="fas fa-laugh-wink"></i>
         </div>
-        <div class="sidebar-brand-text mx-3">SB Admin <sup>2</sup></div>
+        <div class="sidebar-brand-text mx-3">Thats My Spot</div>
     </a>
 
     <!-- Divider -->
     <hr class="sidebar-divider my-0">
-    
+
     <!-- Nav Item - Dashboard -->
     <li class="nav-item active">
-        <a class="nav-link" href="index.html">
+        <a class="nav-link" href="index.php">
             <i class="fas fa-fw fa-tachometer-alt"></i>
             <span>Dashboard</span></a>
     </li>
@@ -34,16 +34,16 @@
 
     <!-- Heading -->
     <div class="sidebar-heading">
-        Interface
+        Manage
     </div>
-<?php if($u['ACESS'] == "Admin"){?>
+    <?php if($u['ACESS'] == "Admin"){?>
     <!-- Nav Item - Pages Collapse Menu -->
     <li class="nav-item">
-        <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="true"
-            aria-controls="collapseTwo">
+        <a class="nav-link" href="user_manage.php">
             <i class="fas fa-fw fa-cog"></i>
-            <span>Components</span>
+            <span>Users</span>
         </a>
+        <!--
         <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
             <div class="bg-white py-2 collapse-inner rounded">
                 <h6 class="collapse-header">Custom Components:</h6>
@@ -51,15 +51,16 @@
                 <a class="collapse-item" href="cards.html">Cards</a>
             </div>
         </div>
+        -->
     </li>
-<?php }?>
+    <?php }?>
     <!-- Nav Item - Utilities Collapse Menu -->
     <li class="nav-item">
-        <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseUtilities"
-            aria-expanded="true" aria-controls="collapseUtilities">
-            <i class="fas fa-fw fa-wrench"></i>
-            <span>Utilities</span>
+        <a class="nav-link" href="place_manage.php">
+            <i class="fas fa-fw fa-cog"></i>
+            <span>Parking Places</span>
         </a>
+        <!--
         <div id="collapseUtilities" class="collapse" aria-labelledby="headingUtilities" data-parent="#accordionSidebar">
             <div class="bg-white py-2 collapse-inner rounded">
                 <h6 class="collapse-header">Custom Utilities:</h6>
@@ -69,6 +70,7 @@
                 <a class="collapse-item" href="utilities-other.html">Other</a>
             </div>
         </div>
+        -->
     </li>
 
     <!-- Divider -->
@@ -76,7 +78,7 @@
 
     <!-- Heading -->
     <div class="sidebar-heading">
-        Addons
+        DSS
     </div>
 
     <!-- Nav Item - Pages Collapse Menu -->
@@ -84,18 +86,14 @@
         <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapsePages" aria-expanded="true"
             aria-controls="collapsePages">
             <i class="fas fa-fw fa-folder"></i>
-            <span>Pages</span>
+            <span>Reports</span>
         </a>
         <div id="collapsePages" class="collapse" aria-labelledby="headingPages" data-parent="#accordionSidebar">
             <div class="bg-white py-2 collapse-inner rounded">
-                <h6 class="collapse-header">Login Screens:</h6>
-                <a class="collapse-item" href="login.html">Login</a>
-                <a class="collapse-item" href="register.html">Register</a>
-                <a class="collapse-item" href="forgot-password.html">Forgot Password</a>
-                <div class="collapse-divider"></div>
-                <h6 class="collapse-header">Other Pages:</h6>
-                <a class="collapse-item" href="404.html">404 Page</a>
-                <a class="collapse-item" href="blank.html">Blank Page</a>
+                <a class="collapse-item" href="#">Users</a>
+                <a class="collapse-item" href="#">Parking Places</a>
+                <a class="collapse-item" href="#">Authentication Log</a>
+                <a class="collapse-item" href="#">Used Times</a>
             </div>
         </div>
     </li>
@@ -111,7 +109,7 @@
     <li class="nav-item">
         <a class="nav-link" href="tables.html">
             <i class="fas fa-fw fa-table"></i>
-            <span>Tables</span></a>
+            <span>Forecasts</span></a>
     </li>
 
     <!-- Divider -->
