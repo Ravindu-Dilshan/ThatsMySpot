0dc431b205af8fea112efa80f029f20eac6abbb9
@Ruke45 <madusankagmt@gmail.com>
2020-08-26 13:38:00 +0530

    no message

--- userapp/home.php
+++ userapp/home.php
@@ -42,6 +42,7 @@ if(isset($_SESSION['loggedUser'])){
 
 <body>
     <?php
+    //this is the end
     include('../script/class/payment.cls.php');
     $pay = new Payment(null,null,$u['UID'],null);
     $count = $pay->getUnpaidCount();
